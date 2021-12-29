17068f7748149b3ded6d4c76042a7744c3948035
mrwindmark <agentstorm@ya.ru>
2021-12-30 00:38:52 +0300

    Initial commit

--- stickers/stickers/settings.py
+++ stickers/stickers/settings.py
@@ -20,6 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+with open('replacements.txt', 'r', encoding='UTF-8') as file:
+    key = file.readline()
+
 SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
