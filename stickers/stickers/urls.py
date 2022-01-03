c58c8b7837e79972e5cf254a73375741dd570b56
mrwindmark <agentstorm@ya.ru>
2022-01-03 15:00:39 +0300

    React app and CORS for api created and tested

--- stickers/stickers/urls.py
+++ stickers/stickers/urls.py
@@ -19,7 +19,7 @@ from rest_framework.routers import DefaultRouter
 from userapp.views import UserModelViewSet
 
 router = DefaultRouter()
-router.register('user', UserModelViewSet)
+router.register('users', UserModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
