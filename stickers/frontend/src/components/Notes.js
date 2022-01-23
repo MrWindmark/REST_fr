597ae3bdef3e5ca5913f57476f3f9f467411c8fc
mrwindmark <agentstorm@ya.ru>
2022-01-24 00:42:00 +0300

    Routing in use. Tasks for lesson 5 done

---
+++ stickers/frontend/src/components/Notes.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+const NoteItem = ({note}) => {
+    return (
+        <tr>
+            <td>{note.title}</td>
+            <td>{note.inner_text}</td>
+            <td>{note.created_at}</td>
+            <td>{note.Date}</td>
+            <td>{note.is_complited.toString()}</td>
+            <td>{note.creator.username}</td>
+        </tr>
+    )
+}
+
+const NotesList = ({notes}) => {
+    return (
+        <table>
+            <th>Название</th>
+            <th>Описание</th>
+            <th>Дата создания</th>
+            <th>Дата завершения</th>
+            <th>Статус</th>
+            <th>Создатель</th>
+            {notes.map((elem) => <NoteItem note={elem}/>)}
+        </table>
+    )
+}
+
+export default NotesList
